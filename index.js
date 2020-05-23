abfc6392289c1e6e20fe52a5fe100889d2c0aed4
gietpa <unvierpark@naver.com>
2020-05-24 01:10:25 +1000

    First time

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const config = require("./config/key");
+
+const { User } = require("./models/user");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/jason
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongURI, {
+    useNewUrlParser: true,
+    useUnifieldTopology: true,
+    useCreateIndex: true,
+    useFinAndModify: false,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World! Hi"));
+
+app.post("/register", (req, res) => {
+  //Put into db from client
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  //check requested email in DB
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "Not matched User Name",
+      });
+    }
+    //check requested password and if it is correct
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "Wrong password" });
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // Save Token
+        res
+          .cookie("x-auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
